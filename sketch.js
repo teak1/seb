15e0e65cea552658371b24cea86ea05aedc20c74
teak1 <trainian@gmail.com>
2017-02-16 10:44:12 -0800

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -4,6 +4,11 @@ function setup(){
 }
 
 function draw(){
-  colorMode(HSV);
-  background(bgColor);
+  bgColor++;
+  var r=255,g=255,b=255;
+  if(bgColor>255)r=bgColor;
+  if(bgColor>255 && bgColor<510)b=bgColor-255;
+  if(bgColor>510 && bgColor<765)g=bgColor-510;
+  if(bgColor>765)bgColor=0;
+  background(r,g,b);
 }
