0777630423e7a148af8e38582ca59cbb6093938c
teak1 <trainian@gmail.com>
2017-02-16 11:30:08 -0800

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -6,9 +6,5 @@ function setup(){
 function draw(){
   bgColor++;
   var r=255,g=255,b=255;
-  if(bgColor>255)r=bgColor;
-  if(bgColor>255 && bgColor<510)b=bgColor-255;
-  if(bgColor>510 && bgColor<765)g=bgColor-510;
-  if(bgColor>765)bgColor=0;
-  background(r,g,b);
+  background(gbColor%123,bgColor%456,bgColor%789);
 }
