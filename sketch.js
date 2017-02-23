edea868aea75822c962970f1e39871870923f48c
teak1 <trainian@gmail.com>
2017-02-23 14:11:32 -0800

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,34 +1,34 @@
-var keys = {};
-var player = {};
-var bgColor = 0;
-function setup(){
-  createCanvas(400,400);
-  player = {
-    pos:createVector(200,200)
-  }
-}
-function onkeyup(e){
-  console.log(e.key.toUpperCase());
-  keys[e.key.toUpperCase()]=true;
-}
-function onkeydown(e){
-  console.log(e.key.toUpperCase());
-  keys[e.key.toUpperCase()]=false;
-}
-function draw(){
-  background(51);
-  if(keys.W){
-    player.pos.x--;
-  }
-  if(keys.S){
-    player.pos.x++;
-  }
-  if(keys.A){
-    player.pos.x--;
-  }
-  if(keys.D){
-    player.pos.x++;
-  }
-  fill(255,255,255);
-  rect(player.pos.x,player.pos.y,10,10);
-}
+// var keys = {};
+// var player = {};
+// var bgColor = 0;
+// function setup(){
+//   createCanvas(400,400);
+//   player = {
+//     pos:createVector(200,200)
+//   }
+// }
+// function onkeyup(e){
+//   console.log(e.key.toUpperCase());
+//   keys[e.key.toUpperCase()]=true;
+// }
+// function onkeydown(e){
+//   console.log(e.key.toUpperCase());
+//   keys[e.key.toUpperCase()]=false;
+// }
+// function draw(){
+//   background(51);
+//   if(keys.W){
+//     player.pos.x--;
+//   }
+//   if(keys.S){
+//     player.pos.x++;
+//   }
+//   if(keys.A){
+//     player.pos.x--;
+//   }
+//   if(keys.D){
+//     player.pos.x++;
+//   }
+//   fill(255,255,255);
+//   rect(player.pos.x,player.pos.y,10,10);
+// }
