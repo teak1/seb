d8b0d4b8d616d1dee0641e65bfd3a2e52f13165f
teak1 <trainian@gmail.com>
2017-02-23 14:03:03 -0800

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,8 +1,30 @@
+var keys = {};
+var player = {};
 var bgColor = 0;
 function setup(){
   createCanvas(400,400);
-  background(255,0,125)
+  player = {
+    pos:createVector(200,200);
+  }
 }
-
+function onkeyup(e){
+  keys[e.key.toUpperCase()]=true;
+}
+function onkeydown(e){
+  keys[e.key.toUpperCase()]=false;
+}
 function draw(){
+  background(51);
+  if(keys.W){
+    player.pos.x--;
+  }
+  if(keys.S){
+    player.pos.x++;
+  }
+  if(keys.A){
+    player.pos.x--;
+  }
+  if(keys.D){
+    player.pos.x++;
+  }
 }
