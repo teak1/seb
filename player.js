fecdb357adf643b77d6618bd4c44ad10e208af78
teak1 <trainian@gmail.com>
2017-02-24 13:40:35 -0800

    Create player.js

---
+++ player.js
@@ -0,0 +1,23 @@
+function Player(x,y,speed){
+  this.pos = createVector(x,y);
+  this.speed = speed || 1;
+}
+Player.prototype = Object.assign(
+Player.prototype,
+{
+  move(){
+    if(keys.W||keys.ARROWUP){
+      this.pos.y-=this.speed;
+    }
+    if(keys.S||keys.ARROWDOWN){
+      this.pos.y+=this.speed;
+    }
+    if(keys.A||keys.ARROWLEFT){
+      this.pos.x-=this.speed;
+    }
+    if(keys.D||keys.ARROWRIGHT){
+      this.pos.x+=this.speed;
+    }
+  }
+}
+);
