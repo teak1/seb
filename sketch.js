c019e3edae2d1ed00d534feabfa79a9fadaebce3
teak1 <trainian@gmail.com>
2017-02-24 14:09:01 -0800

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,6 +1,7 @@
 var keys = {};
 var player = {};
 var bgColor = 0;
+var obs = [new Obstical(0,0,40,40)];
 function setup(){
   createCanvas(400,400);
   player = new Player(width/2,height/2);
@@ -17,4 +18,5 @@ function draw(){
   background(51);
   player.move();
   player.show();
+  for(var i = 0;i<obs.length;i++)obs[i].show();
 }
