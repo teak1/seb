8581624b9c93325b4dba37a8ec440a514c5f8af6
TSseb <staylor@sterneschool.org>
2017-02-27 14:11:08 -0800

    Update player.js

--- player.js
+++ player.js
@@ -1,13 +1,18 @@
 function Player(x,y,speed){
   this.pos = createVector(x,y);
   this.speed = speed || 1;
+  this.size = createVector(10,10);
+  this.obs;
 }
 Player.prototype = Object.assign(
 Player.prototype,
 {
   move(){
+    this.obs = obs;
     if(keys.W||keys.ARROWUP){
-      this.pos.y-=this.speed;
+      if(this.checkCollision(createVector(0,-this.speed))){
+        this.pos.y-=this.speed;
+      }
     }
     if(keys.S||keys.ARROWDOWN){
       this.pos.y+=this.speed;
@@ -19,9 +24,24 @@ Player.prototype,
       this.pos.x+=this.speed;
     }
   },
-  show(){
+  show()
+  {
     fill(255);
-    rect(this.pos.x-5,this.pos.y-5,10,10);
+    rect(this.pos.x-5,this.pos.y-5,this.size.x,this.size.y);
+  }
+  , checkCollison(offset){ 
+    for(var i = 0;i<this.obs.length;i++){
+      var current = this.obs[i];
+      var t = {pos:this.pos.add(offset),size:this.size}
+      var cBB = current.getBoundingBox();
+      if(t.pos.x+t.size.x>cBB.pos.x&&
+        cBB.pos.x+cBB.size.x<t.pos.x&&
+        t.pos.y+t.size.y>cBB.pos.y&&
+        cBB.pos.y+cBB.size.y<t.pos.y ){
+        return true;
+      }
+    }
+    return false;
   }
 }
 );
